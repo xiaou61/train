601a281fbe8a8615ce9edbe8a14eb42a371a78f7
李子凡 <3153566913@qq.com>
2023-09-01 09:15:11 +0800

    初始化

---
+++ src/main/java/com/xiaou/train/TestController.java
@@ -0,0 +1,12 @@
+package com.xiaou.train;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
