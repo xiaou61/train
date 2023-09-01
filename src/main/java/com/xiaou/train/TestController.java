f83cff4e584f4029b3257e66142cb0cec7173f51
李子凡 <3153566913@qq.com>
2023-09-01 09:20:21 +0800

    1

--- src/main/java/com/xiaou/train/TestController.java
+++ src/main/java/com/xiaou/train/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World1";
+        return "Hello World";
     }
 }
