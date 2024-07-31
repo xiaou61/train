a57e635fed764c54e886a866dc07b840ae909987
aliyun1153623907 <3153566913@qq.com>
2024-07-31 14:18:28 +0800

    初始化代码

---
+++ src/views/welcome/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ res }}</h1>
+</template>
+
+<script setup>
+import { http } from "@/utils/http";
+import { ref } from "vue";
+
+const res = ref();
+// params传参
+const textRequest = async () => {
+  const response = await http.request("get", "/api");
+  res.value = response.data;
+};
+textRequest();
+</script>
